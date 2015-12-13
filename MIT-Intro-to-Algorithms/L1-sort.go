1c4f16066a34fda0361f9322c9a5c7eda2a0a97e
Cole R Lawrence <cole@zombiehippie.com>
2015-12-13 13:00:18 -0600

    Format to common golang lint

--- MIT-Intro-to-Algorithms/L1-sort.go
+++ MIT-Intro-to-Algorithms/L1-sort.go
@@ -2,13 +2,13 @@ package GoAlgorithmsMIT
 
 // Interface borrowed from sort package
 type Interface interface {
-		// Len is the number of elements in the collection.
-		Len() int
-		// Less reports whether the element with
-		// index i should sort before the element with index j.
-		Less(i, j int) bool
-		// Swap swaps the elements with indexes i and j.
-		Swap(i, j int)
+	// Len is the number of elements in the collection.
+	Len() int
+	// Less reports whether the element with
+	// index i should sort before the element with index j.
+	Less(i, j int) bool
+	// Swap swaps the elements with indexes i and j.
+	Swap(i, j int)
 }
 
 /* Pseudocode from lecture
@@ -23,15 +23,10 @@ Insertion-Sort(A, n)
 */
 // A, is the array (uses Interface)
 // n, is the last indice to be sorted of the array
-func InsertionSort (A Interface, a, b int) {
-	for j := a + 1;
-		j <= b;
-		j++ {
-		for i := j - 1;
-			i >= a && A.Less(j, i);
-			i-- {
-			A.Swap(i + 1, i)
+func InsertionSort(A Interface, a, b int) {
+	for j := a + 1; j <= b; j++ {
+		for i := j - 1; i >= a && A.Less(j, i); i-- {
+			A.Swap(i+1, i)
 		}
 	}
 }
-
